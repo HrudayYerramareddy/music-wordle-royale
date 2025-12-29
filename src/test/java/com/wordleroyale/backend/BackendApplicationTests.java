c184099ea08f6b0811eed4271c71a45c688fccbd
hyerram <hyerram@ncsu.edu>
2025-12-29 12:15:49 -0500

    Initial Spring Boot backend setup

---
+++ src/test/java/com/wordleroyale/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordleroyale.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
